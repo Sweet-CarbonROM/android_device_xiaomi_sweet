a3a3ccd0115a42a69ddc6ebd2feb7804aef9f2d1
Ali Hasan <ahb7671@gmail.com>
2022-12-26 20:55:54 +0500

    sweet: initial CarbonROM bringup
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- carbon_sweet.mk
+++ carbon_sweet.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
 # Inherit from those products. Most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -11,7 +14,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-PRODUCT_NAME := jarvis_sweet
+# Inherit common.
+$(call inherit-product, vendor/carbon/config/common.mk)
+
+PRODUCT_NAME := carbon_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
